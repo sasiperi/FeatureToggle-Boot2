b6172f29244a34b72ed3b21088309f8a35df545f
sasiperi <pvssasikala@gmail.com>
2019-10-03 00:52:09 -0400

    flipper db sql

---
+++ flipper-web-app/src/db/flipper_web.sql
@@ -0,0 +1,103 @@
+-- MySQL dump 10.13  Distrib 5.7.18, for Win64 (x86_64)
+--
+-- Host: localhost    Database: demo_webapp
+-- ------------------------------------------------------
+-- Server version	5.7.18
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authorities` (
+  `username` varchar(50) DEFAULT NULL,
+  `authority` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authorities`
+--
+
+LOCK TABLES `authorities` WRITE;
+/*!40000 ALTER TABLE `authorities` DISABLE KEYS */;
+INSERT INTO `authorities` VALUES ('sasi','ROLE_USER'),('peri','ROLE_ADMIN'),('sasi','ROLE_BETA-TESTER');
+/*!40000 ALTER TABLE `authorities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_roles` (
+  `user_role_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_role_id`),
+  UNIQUE KEY `uni_username_role` (`role`,`username`),
+  KEY `fk_username_idx` (`username`),
+  CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+INSERT INTO `user_roles` VALUES (2,'peri','ADMIN'),(4,'peri','ROLE_ADMIN'),(3,'sasi','ROLE_USER'),(1,'sasi','USER');
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(300) DEFAULT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('peri','$2a$10$IXpIkXmHIGE0HYIAA/LaauF5Q.J.djUqxRS9buX/vDjgoGD9rgwAu',1),('sasi','$2a$10$Xh3LTxnXRD7EhQp5p5lzFOCnviDeItlHJwY27CXk6hdoxn41N7C8m',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-10-03  0:50:44
--- flipper-web-app/src/test/java/com/sasip/InventorWebAppApplicationTests.java
+++ flipper-web-app/src/test/java/com/sasip/InventorWebAppApplicationTests.java
@@ -1,16 +1,114 @@
 package com.sasip;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.Map;
+
+import org.ff4j.FF4j;
+import org.ff4j.core.Feature;
+import org.ff4j.exception.FeatureNotFoundException;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-public class InventorWebAppApplicationTests {
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+//@ContextConfiguration(classes = {AnnotationConfigContextLoader.class})
+public class InventorWebAppApplicationTests 
+{
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Autowired
+    FF4j ff4j;
+    
+    @Test
+    //@Ignore
+    public void contextLoads() {
+    }
+    
+    
+    @Test
+    //@Ignore
+    public void my_first_test() {
+        // Given: file exist
+        System.out.println(getClass().getClassLoader().getResource(".").toString());
+        //assertNotNull(getClass().getClassLoader().getResourceAsStream("ff4j.xml"));
+        // When: init
+        //FF4j ff4j = new FF4j("ff4j.xml");
+        for(Map.Entry<String, Feature> entry : ff4j.getFeatures().entrySet())
+        {
+            System.out.println(entry.getKey() + " --- " + entry.getValue().getUid());
+            //System.out.println();
+        }
+        // Then
+        assertEquals(10, ff4j.getFeatures().size());
+        assertTrue(ff4j.exist("AwesomeFeature"));
+        assertTrue(ff4j.check("AwesomeFeature"));
+        
+        System.out.println(ff4j.exist("sasi-f1"));
+        System.out.println(ff4j.check("sasi-f1"));
+        
+        // Usage
+        if (ff4j.check("AwesomeFeature")) {
+            // hello is toggle on
+            System.out.println("AwesomeFeature");
+        }
+        
+        // When: Toggle Off
+        ff4j.disable("AwesomeFeature");
+        // Then: expected to be off
+        assertFalse(ff4j.check("AwesomeFeature"));
+    }
+    
+    @Test
+    @Ignore
+    public void how_does_AutoCreate_Works() {
+        // Given: feature not exist
+        //FF4j ff4j = new FF4j("ff4j.xml");
+        FF4j ff4j = new FF4j();
+        assertFalse(ff4j.exist("does_not_exist"));
+        
+        // When: use it
+        try {
+           if (ff4j.check("does_not_exist")) fail();
+        } catch (FeatureNotFoundException fnfe) {
+            // Then: exception
+            System.out.println("By default, feature not found throw exception");
+        }
+        
+        // When: using autocreate
+        ff4j.setAutocreate(true);
+        // Then: no more exceptions
+        if (!ff4j.check("does_not_exist")) {
+            System.out.println("Auto created and set as false");
+        }
+    }
+    
+    @Test
+    @Ignore
+    public void how_groups_works() {
+        // Given: 2 features 'off' within existing group
+        FF4j ff4j = new FF4j("ff4j.xml");
+        assertTrue(ff4j.exist("userStory3_1"));
+        assertTrue(ff4j.exist("userStory3_2"));
+        assertTrue(ff4j.getFeatureStore().readAllGroups().contains("sprint_3"));
+        assertEquals("sprint_3", ff4j.getFeature("userStory3_1").getGroup());
+        assertEquals("sprint_3", ff4j.getFeature("userStory3_2").getGroup());
+        assertFalse(ff4j.check("userStory3_1"));
+        assertFalse(ff4j.check("userStory3_2"));
+  
+        // When: toggle group on
+        ff4j.getFeatureStore().enableGroup("sprint_3");
+        
+        // Then: all features on
+        assertTrue(ff4j.check("userStory3_1"));
+        assertTrue(ff4j.check("userStory3_2"));
+    }
 }
+
